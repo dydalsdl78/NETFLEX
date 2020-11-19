15eea5e32b1c460ed33614ff75892ac1d62ae3aa
dydalsdl78 <dydalsdl78@gmail.com>
2020-11-19 15:49:19 +0900

    201119

--- final_pjt_back/movies/models.py
+++ final_pjt_back/movies/models.py
@@ -2,7 +2,21 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-class Movie(models.Model):
-    title = models.CharField(max_length=50)
 
-    
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+class Movie(models.Model):
+    popularity = models.FloatField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
+    overview = models.TextField()
+    poster_path = models.CharField(max_length=200)
+    title = models.CharField(max_length=50)
+    release_date = models.DateField()
+    video = models.BooleanField()
+    adult = models.BooleanField()
+    backdrop_path = models.CharField(max_length=200, null=True)
+    original_language = models.CharField(max_length=10)
+    original_title = models.CharField(max_length=50)
+    genre_ids = models.ManyToManyField(Genre)
--- final_pjt_back/movies/tmdb.py
+++ final_pjt_back/movies/tmdb.py
@@ -5,16 +5,29 @@ api_key = "d7a0f6399832ff632e1b02fc2afb5d21"
 
 data = []
 
-for page in range(1, 11):
-    url = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key + "&language=ko-KR&page=" + str(page)
+# with open('./fixtures/genre.json', encoding='UTF-8') as json_file:
+#     genre_data = json.load(json_file)
 
+# data.append(genre_data)
+# print(data)
+
+for page in range(0, 10):
+    url = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key + "&language=ko-KR&page=" + str(page + 1)
+    # 응답
     response = requests.get(url)
-
+    # 응답값 저장
     tmp = json.loads(response.text)
 
-    data.extend(tmp['results'])
+    # 한번에 20개의 데이터를 반복문을 돌면서
+    for i in range(20):
+        # print(i)
+        # 한개의 딕셔너리 안에 "model" : "movies.movie" 라는 키, 값 형태로 저장
+        data.append({"model": "movies.movie"})
+        # 전체 필드를 "fields": tmp[results][i] 키, 값 형태로 저장
+        data[20 * page + i]["fields"] = tmp['results'][i]
 
-file_path = "./movies.json"
+
+file_path = "./fixtures/movies.json"
 
 with open(file_path, 'w', encoding='UTF-8') as outfile:
     json.dump(data, outfile, ensure_ascii=False, indent=4)
