c7631489ad938a3202dc6f3309f2274d976b0a5f
Kihyeon KWON <54575770+kihyeon04@users.noreply.github.com>
2020-11-26 00:07:46 +0900

    1126 merge success 기현

--- final_pjt_back/movies/urls.py
+++ final_pjt_back/movies/urls.py
@@ -11,9 +11,5 @@ urlpatterns = [
     path('getgenre/', views.getGenre),
     path('<int:review_pk>/', views.detail),
     path('<int:review_pk>/comment_crud/', views.comment_crud),
-<<<<<<< HEAD
-    path('moviedetail/', views.movieDetail)
-=======
     path('get_movie/', views.get_movie),
->>>>>>> 203d3d205037eb3aab76571eee1df1d04fc30f6d
 ]
