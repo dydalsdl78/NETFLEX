fd67e9a7f2e511a9ebbc2feec467cde2553016e1
Kihyeon KWON <54575770+kihyeon04@users.noreply.github.com>
2020-11-25 16:33:22 +0900

    1125 오후 merge success!

--- final_pjt_front/src/views/Community.vue
+++ final_pjt_front/src/views/Community.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="Community">
     <h2>Community</h2>
-    
-    <Username />
     <ReviewList />
   </div>
 </template>
 
 <script>
-import Username from '@/views/Username'
 import ReviewList from '@/components/ReviewList'
 
 export default {
   name:'Community',
-  components: {Username, ReviewList},
+  components: {ReviewList},
 };
 </script>
 
